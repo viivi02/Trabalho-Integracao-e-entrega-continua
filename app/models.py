7c4e39dfed3102b3c7169e5edb95deb23ac8bb1a
viivi02 <vinicius.lemes3938@gmail.com>
2025-05-29 16:03:01 -0300

    subindo primeiro commit

---
+++ app/models.py
@@ -0,0 +1,6 @@
+from . import db
+
+class Pessoa(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100))
+    email = db.Column(db.String(120), unique=True)
